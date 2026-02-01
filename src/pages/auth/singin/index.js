8cc3fd8822054f8477791f7a766dde181f0ba9c9
Daniel Wong <dWong19991225@gmail.com>
2026-02-01 08:14:39 -0800

    feature: change signin page

--- src/pages/auth/singin/index.js
+++ src/pages/auth/singin/index.js
@@ -1,11 +1,22 @@
 import { CustomInput } from '../../../components/customInput';
+import { CustomBtn } from '../../../components/customBtn';
+
+import smallLogo from '../../../images/logo/logo2.png';
 
 import './signin.scss';
 
 const Signin = () => {
   return (
     <div className="signin-page">
-      <div className="signin-header"></div>
+      <div className="signin-header">
+        <div className="logo">
+          <img src={smallLogo} alt="small-logo" />
+        </div>
+        <div className="auth-status">
+          <CustomBtn value={'Log In'} />
+          <CustomBtn value={'Sign Up'} />
+        </div>
+      </div>
       <div className="signin-body"></div>
     </div>
   );

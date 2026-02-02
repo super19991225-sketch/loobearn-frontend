8ba6f27628bd1cb4387c80b6c1704bc0d6b2dac7
Daniel Wong <dWong19991225@gmail.com>
2026-02-01 18:47:45 -0800

    change: change signin-page

--- src/pages/auth/singin/index.js
+++ src/pages/auth/singin/index.js
@@ -1,11 +1,17 @@
+import { useEffect, useState } from 'react';
+
 import { CustomInput } from '../../../components/customInput';
 import { CustomBtn } from '../../../components/customBtn';
 
 import smallLogo from '../../../images/logo/logo2.png';
+import vector from '../../../images/vector/Vector.png';
 
 import './signin.scss';
 
 const Signin = () => {
+  const [degree, setDeg] = useState([]);
+  useEffect(() => {}, []);
+
   return (
     <div className="signin-page">
       <div className="signin-header">
@@ -17,7 +23,18 @@ const Signin = () => {
           <CustomBtn value={'Sign Up'} />
         </div>
       </div>
-      <div className="signin-body"></div>
+      <div className="signin-body">
+        <div className="signin-logo">
+          <div className="vectors">
+            <img
+              src={vector}
+              alt="vector"
+              style={{ transform: 'rotate(30deg)' }}
+            />
+          </div>
+        </div>
+        <div className="signin-input"></div>
+      </div>
     </div>
   );
 };

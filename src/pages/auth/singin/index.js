892cc57cd0089abdcd20927a5a982fa4e8b797d1
Daniel Wong <dWong19991225@gmail.com>
2026-02-03 04:39:24 -0800

    feature: change the background

--- src/pages/auth/singin/index.js
+++ src/pages/auth/singin/index.js
@@ -9,8 +9,14 @@ import vector from '../../../images/vector/Vector.png';
 import './signin.scss';
 
 const Signin = () => {
-  const [degree, setDeg] = useState([]);
-  useEffect(() => {}, []);
+  const [degrees, setDeg] = useState([]);
+  useEffect(() => {
+    var array = [];
+    for (let i = -75; i < 0; i += 3) {
+      array.push(i);
+    }
+    setDeg(array);
+  }, []);
 
   return (
     <div className="signin-page">
@@ -26,11 +32,17 @@ const Signin = () => {
       <div className="signin-body">
         <div className="signin-logo">
           <div className="vectors">
-            <img
-              src={vector}
-              alt="vector"
-              style={{ transform: 'rotate(30deg)' }}
-            />
+            {degrees.map((degree, idx) => (
+              <img
+                src={vector}
+                alt="vector"
+                key={idx}
+                style={{
+                  transform: `rotateX(${degree}deg) rotateY(${degree - 10}deg)`,
+                  opacity: `${-degree / 75}`
+                }}
+              />
+            ))}
           </div>
         </div>
         <div className="signin-input"></div>
